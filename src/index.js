e1b509aa7a843da070652a47c8a19f5fbef94123
ytt <630880770@qq.com>
2018-08-06 10:40:08 +0800

    first init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+export default function Say(){
+    console.log('====================================');
+    console.log('hello world');
+    console.log('====================================');
+}
